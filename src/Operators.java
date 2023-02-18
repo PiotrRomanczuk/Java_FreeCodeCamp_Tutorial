bc3bb8ef2df22a1be3c030ee0a8a4294384fe8ae
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-02-18 23:12:35 +0100

    Adding java Datatypes

---
+++ src/Operators.java
@@ -0,0 +1,21 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        int number1 = 4;
+        int number2 = 9;
+        int number3 = number1;
+
+
+        System.out.println("Number is 1 equal: " + number1);
+        System.out.println("Number is 2 equal: " + number2);
+        System.out.println("Number is 3 equal - number1 * number2: " + number3);
+
+
+        // Is equal to 
+        System.out.println();
+        System.out.println(number1 == number2);
+
+
+
+    }
+}
