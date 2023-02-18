ddb4d60ade3abfff363d77ae02ff0576c018848c
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-02-18 23:33:12 +0100

    Added Strings.java

---
+++ src/Strings.java
@@ -0,0 +1,17 @@
+public class Strings {
+    public static void main(String[] args) {
+
+        String name = "Piotr Romanczuk";
+        String country = "Poland";
+        int age = 27;
+        String yt_channel = "FreeCodeCamp";
+
+        String formattedString = String.forma
+
+
+        System.out.println("My name is " + name + ", I am from " + country + " and I am learning Java from YT " + yt_channel);
+
+
+
+    }
+}
